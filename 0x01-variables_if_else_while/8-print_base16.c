663bf79db4965851fd707c8c473ccb8f28f9267f
Fazazi Abanikanda <abiodunabanikanda2@gmail.com>
2023-02-17 19:39:04 -0800

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints from 0 to 15.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
